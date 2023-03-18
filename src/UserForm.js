44284d84e83cf837396b2701a1546ff5c407f07c
Saad Koubeissi <saad@koubeissi.com>
2023-03-18 17:10:29 +0400

    feat: added a boilerplate project to start testing

---
+++ src/UserForm.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function UserForm({ onUserAdd }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    onUserAdd({ name, email });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Name</label>
+        <input value={name} onChange={e => setName(e.target.value)} />
+      </div>
+      <div>
+        <label>Email</label>
+        <input value={email} onChange={e => setEmail(e.target.value)} />
+      </div>
+      <button>Add User</button>
+    </form>
+  )
+}
+
+export default UserForm;
